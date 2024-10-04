5d7dc63f451f4cde9956a9aaec7e0f5eb699fe6b
Palnit <105753906+Palnit@users.noreply.github.com>
2024-10-04 21:04:13 +0200

    Testing Setup

---
+++ test/src/main.cpp
@@ -0,0 +1,16 @@
+#include "general/main_window.h"
+#include "general/cuda/gpu_info.h"
+
+int main(int argc, char* args[]) {
+    GetGpuInfo();
+
+    MainWindow win("Edge Detector Test",
+                   SDL_WINDOWPOS_CENTERED,
+                   SDL_WINDOWPOS_CENTERED,
+                   1024,
+                   720,
+                   SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+                       | SDL_WINDOW_RESIZABLE);
+
+    return win.run();
+}
