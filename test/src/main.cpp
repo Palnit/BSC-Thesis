6d84b4de39380aef7574ca81083e1ce66b8dc8ce
Palnit <105753906+Palnit@users.noreply.github.com>
2024-10-20 23:36:57 +0200

    Testing wip

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <random>
+#include <numeric>
 #include "SDL_image.h"
 #include "general/OpenGL_SDL/generic_structs.h"
 #include "glm/vec2.hpp"
@@ -41,6 +42,12 @@ private:
     int m_m = 1;
 };
 
+float DistanceOfPixels(int x1, int y1, int x2, int y2) {
+    int x = (x2 - x1) * (x2 - x1);
+    int y = (y2 - y1) * (y2 - y1);
+    return std::sqrtf(x + y);
+}
+
 int main(int argc, char* args[]) {
     if (IMG_Init(IMG_INIT_JPG) == 0) {
         ErrorHandling::HandelSDLError("SDL IMG initialization");
@@ -80,9 +87,8 @@ int main(int argc, char* args[]) {
         std::uniform_int_distribution<std::mt19937::result_type> dist(0, 255);
         RGBA* color = (RGBA*) (((uint8_t*) m_base->pixels) + ((int) a.x * 4)
             + ((int) a.y * m_base->w * 4));
-        color->r = dist(rng);
-        color->g = dist(rng);
-        color->b = dist(rng);
+        color->r = 255;
+        color->b = 0;
         if (a.x == b.x && a.y == b.y) { break; }
         float e2 = 2 * error;
         if (e2 >= dy) {
@@ -173,9 +179,6 @@ int main(int argc, char* args[]) {
     IMG_SavePNG(m_detected2, "./dog_detected.png");
 
     IMG_SavePNG(m_detected, "./detected.png");
-    SDL_FreeSurface(m_base);
-    SDL_FreeSurface(m_detected);
-    SDL_FreeSurface(m_detected2);
 
     SDL_Surface
         * loadedSurface2 = SDL_CreateRGBSurface(0, 100, 100, 32, 0, 0, 0, 0);
@@ -187,6 +190,59 @@ int main(int argc, char* args[]) {
         std::cout << "x: " << indexer.X() << " Y: " << indexer.Y() << std::endl;
         indexer++;
     }
+    std::vector<float> distances;
+
+    for (int x = 0; x < m_detected->w; ++x) {
+        for (int y = 0; y < m_detected->h; ++y) {
+            RGBA* color = (RGBA*) (((uint8_t*) m_detected2->pixels) + (x * 4)
+                + (y * m_detected2->w * 4));
+            if (color->r <= 20) {
+                color->r = color->b = color->g = 0;
+            }
+        }
+    }
+
+    for (int x = 0; x < m_detected->w; ++x) {
+        for (int y = 0; y < m_detected->h; ++y) {
+            RGBA* color = (RGBA*) (((uint8_t*) m_detected2->pixels) + (x * 4)
+                + (y * m_detected2->w * 4));
+            if (color->r != 0 && color->b != 0 && color->g != 0) {
+                SpiralIndexer indexer2;
+                bool match = false;
+                for (int i = 0; i < 25; i++) {
+                    int nX = x + indexer2.X();
+                    int nY = y + indexer2.Y();
+                    if (nX >= m_detected->w || nY >= m_detected->h) {
+                        indexer2++;
+                        continue;
+                    }
+
+                    RGBA* color2 =
+                        (RGBA*) (((uint8_t*) m_base->pixels) + (nX * 4)
+                            + (nY * m_base->w * 4));
+                    if (color2->r == 255) {
+                        float dis = DistanceOfPixels(x, y, nX, nY);
+                        std::cout << "Dis : " << dis << std::endl;
+                        distances.push_back(dis);
+                        match = true;
+                        break;
+                    }
+                    indexer2++;
+
+                }
+                if (!match) {
+                    std::cout << "FUCK YOU" << std::endl;
+                }
+            }
+        }
+    }
+    auto avg = std::reduce(distances.begin(), distances.end())
+        / (float) distances.size();
+    std::cout << "AVG : " << avg << std::endl;
+
+    SDL_FreeSurface(m_base);
+    SDL_FreeSurface(m_detected);
+    SDL_FreeSurface(m_detected2);
 
     return 0;
 
