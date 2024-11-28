702f9b3db1b2b0276399b06ca76e6db93927dbe7
Palnit <105753906+Palnit@users.noreply.github.com>
2024-11-28 14:05:13 +0100

    cuda linux fixes

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,7 +1,16 @@
-#include "testing_window.h"
 #include "general/OpenCL/get_devices.h"
+#include "testing_window.h"
+
+#ifdef CUDA_EXISTS
+#include "general/cuda/gpu_info.h"
+#endif
+
 
 int main(int argc, char* args[]) {
+#ifdef CUDA_EXISTS
+    GetGpuInfoCuda();
+#endif
+
     TestingWindow win(
         "Testing Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024,
         720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
